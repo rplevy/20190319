54a2cea561c2868cf562669408b2d9397b6e3191
Robert P. Levy <r.p.levy@gmail.com>
2019-03-20 00:45:13 -0700

    Implement validation functionality

--- src/assignment/validate.clj
+++ src/assignment/validate.clj
@@ -2,13 +2,13 @@
   "Validate input data in three supported formats")
 
 (defn pipe-delimited? [line]
-  )
+  (re-matches #".*\|.*\|.*\|.*\|.*" line))
 
 (defn comma-delimited? [line]
-  )
+  (re-matches #".*,.*,.*,.*,.*" line))
 
 (defn space-delimited? [line]
-  )
+  (re-matches #"[^\s]*\s[^\s]*\s[^\s]*\s[^\s]*\s[^\s]*" line))
 
 (defn detect-format
   "either produce a keyword for the format or nil (invalid)"
--- test/assignment/validate_test.clj
+++ test/assignment/validate_test.clj
@@ -3,9 +3,9 @@
             [assignment.validate :as base]))
 
 (deftest validation-tests
-  (is (not (base/pipe-delimited? "Wilson | Freddie | male | beige | 5/2/1975")))
-  (is (not (base/comma-delimited? "Moore, Harry, male, pink, 9/2/2015")))
-  (is (not (base/space-delimited? "Davis Emily female turquoise 9/8/2010")))
+  (is (base/pipe-delimited? "Wilson | Freddie | male | beige | 5/2/1975"))
+  (is (base/comma-delimited? "Moore, Harry, male, pink, 9/2/2015"))
+  (is (base/space-delimited? "Davis Emily female turquoise 9/8/2010"))
   (is (= :space-delimited
          (base/detect-format "Davis Emily female turquoise 9/8/2010")))
   (is (= :comma-delimited
